a2995eef1855b060333ebed2cee8947d4c8baadb
Donovan-T8 <48929580+Donovan-T8@users.noreply.github.com>
2019-04-29 20:39:28 -0400

    Update Pong.py - Donovan
    Altered speed of ball to be faster for higher difficulty
    Changed the speed of paddle movement to be faster to account for the ball speed
    Removed second paddle from the game
    Changed the "score" print 
    In the process of changing code to made ball bounce off of it  (See line 152-154)

--- Pong.py
+++ Pong.py
@@ -2,85 +2,99 @@
 
 import turtle
 
-wn = turtle.Screen()
-wn.title("Flashback Pong")
-wn.bgcolor("black")
-wn.setup(width=800, height=600)
-wn.tracer(0)
+screen = turtle.Screen()
+screen.title("Flashback Pong")
+screen.bgcolor("white")
+screen.setup(width=800, height=600)
+screen.tracer(0)
 
 # Score
-score_a = 0
+#score_a = 0
 score_b = 0
 
 # Paddle A
-paddle_a = turtle.Turtle()
-paddle_a.speed(0)
-paddle_a.shape("square")
-paddle_a.color("yellow")
-paddle_a.shapesize(stretch_wid=5,stretch_len=1)
-paddle_a.penup()
-paddle_a.goto(-350, 0)
+# paddle_a = turtle.Turtle()
+# paddle_a.speed(0)
+# paddle_a.shape("square")
+# paddle_a.color("blue") #changed paddle A to blue
+# paddle_a.shapesize(stretch_wid=5,stretch_len=1)
+# paddle_a.penup()
+# paddle_a.goto(-350, 0) #paddle A location
 
-# Paddle B
+# Paddle B (The paddle we are changing to the bottom of screen)
 paddle_b = turtle.Turtle()
-paddle_b.speed(0)
+paddle_b.speed(0) #paddle speed 
 paddle_b.shape("square")
-paddle_b.color("yellow")
-paddle_b.shapesize(stretch_wid=5,stretch_len=1)
+paddle_b.color("red") #changed paddle B to red
+paddle_b.shapesize(stretch_wid=1,stretch_len=5)
 paddle_b.penup()
-paddle_b.goto(350, 0)
+paddle_b.goto(0, -200) #location of paddle B
 
 # Ball
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("square")
-ball.color("blue")
+ball.color("black") #changed ball color to black
 ball.penup()
-ball.goto(0, 0)
-ball.dx = 2
-ball.dy = 2
+ball.goto(0, 200)
+ball.dx = 3.5
+ball.dy = 3.5
 
 # Pen
 pen = turtle.Turtle()
 pen.speed(0)
 pen.shape("square")
-pen.color("black")
+pen.color("green")
 pen.penup()
 pen.hideturtle()
 pen.goto(0, 260)
-pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+pen.write("Score: {}".format(score_b), align="center", font=("Courier", 24, "normal")) #code for title and scoreboard
 
 # Function
-def paddle_a_up():
-    y = paddle_a.ycor()
-    y += 20
-    paddle_a.sety(y)
+# def paddle_a_up():
+#     y = paddle_a.ycor()
+#     y += 20
+#     paddle_a.sety(y)
 
-def paddle_a_down():
-    y = paddle_a.ycor()
-    y -= 20
-    paddle_a.sety(y)
+# def paddle_a_down():
+#     y = paddle_a.ycor()
+#     y -= 20
+#     paddle_a.sety(y)
 
-def paddle_b_up():
-    y = paddle_b.ycor()
-    y += 20
-    paddle_b.sety(y)
+# def paddle_b_up():
+#   y = paddle_b.ycor()
+#   y += 20
+#   paddle_b.sety(y)
 
-def paddle_b_down():
-    y = paddle_b.ycor()
-    y -= 20
-    paddle_b.sety(y)
+# def paddle_b_down):
+#    y = paddle_b.ycor()
+#    y -= 20
+#    paddle_b.sety(y)
+
+def paddle_b_left():
+    x = paddle_b.xcor()
+    x += -30
+    paddle_b.setx(x)
+
+def paddle_b_right():
+    x = paddle_b.xcor()
+    x += 30
+    paddle_b.setx(x)
+
 
 # Keyboard binding
-wn.listen()
-wn.onkeypress(paddle_a_up, "w")
-wn.onkeypress(paddle_a_down, "s")
-wn.onkeypress(paddle_b_up, "Up")
-wn.onkeypress(paddle_b_down, "Down")
+screen.listen()
+#screen.onkeypress(paddle_a_up, "w")
+#screen.onkeypress(paddle_a_down, "s")
+#screen.onkeypress(paddle_b_up, "Up")
+#screen.onkeypress(paddle_b_down, "Down")
+
+screen.onkeypress(paddle_b_left, "Left")
+screen.onkeypress(paddle_b_right, "Right")
 
 # Main game loop
 while True:
-    wn.update()
+    screen.update()
     
     # Move the ball
     ball.setx(ball.xcor() + ball.dx)
@@ -94,27 +108,47 @@ while True:
             
     elif ball.ycor() < -290:
         ball.sety(-290)
-        ball.dy *= -1
+        ball.dx *= -1
+    
+    #new code for ball to bounce off side walls
+    elif ball.xcor() > 400:
+        ball.setx(400)
+        ball.dx *= -1
+
+    elif ball.xcor() < -400:
+        ball.setx(-400)
+        ball.dx *= -1
 
     # Left and right
-    if ball.xcor() > 350:
-        score_a += 1
-        pen.clear()
-        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
-        ball.goto(0, 0)
-        ball.dx *= -1
+    # if ball.xcor() > 350:
+    #     score_a += 1
+    #     pen.clear()
+    #     pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+    #     ball.goto(0, 0)
+    #     ball.dx *= -1
 
-    elif ball.xcor() < -350:
+    # elif ball.xcor() < -350:
+    #     score_b += 1
+    #     pen.clear()
+    #     pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+    #     ball.goto(0, 0)
+    #     ball.dx *= -1
+
+    if ball.ycor() <- 220:
         score_b += 1
         pen.clear()
-        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
-        ball.goto(0, 0)
+        pen.write("Score: {}".format(score_b), align="center", font=("Courier", 24, "normal"))
+        ball.goto(0,0)
         ball.dx *= -1
-	
+
       # Paddle and ball collisions
-    if ball.xcor() < -340 and ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50:
-        ball.dx *= -1 
+    # if ball.xcor() < -340 and ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50:
+    #     ball.dx *= -1 
             
-    elif ball.xcor() > 340 and ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50:
-        ball.dx *= -1
-        
+    # elif ball.xcor() > 340 and ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50:
+    #     ball.dx *= -1
+    
+    #Paddle and ball collisions for paddle B
+        if ball.ycor() < -190 and ball.xcor() < (paddle_b.xcor() + 50) and ball.xcor() > (paddle_b.xcor() - 50):
+            ball.dy *= -1
+            score_b +=1
