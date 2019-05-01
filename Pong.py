4f2398fd51fe11f68fab559bed47ed42e06af179
Donovan-T8 <48929580+Donovan-T8@users.noreply.github.com>
2019-05-01 17:25:56 -0400

    Fixed collision Issue
    Collision issue with ball and paddle was resolved
    Working on changing score to calculate the number of times user hits the ball instead of how many times they miss

--- Pong.py
+++ Pong.py
@@ -3,23 +3,13 @@
 import turtle
 
 screen = turtle.Screen()
-screen.title("Paddle Arcade")
+screen.title("Flashback Pong")
 screen.bgcolor("white")
 screen.setup(width=800, height=600)
 screen.tracer(0)
 
 # Score
-#score_a = 0
 score_b = 0
-
-# Paddle A
-# paddle_a = turtle.Turtle()
-# paddle_a.speed(0)
-# paddle_a.shape("square")
-# paddle_a.color("blue") #changed paddle A to blue
-# paddle_a.shapesize(stretch_wid=5,stretch_len=1)
-# paddle_a.penup()
-# paddle_a.goto(-350, 0) #paddle A location
 
 # Paddle B (The paddle we are changing to the bottom of screen)
 paddle_b = turtle.Turtle()
@@ -50,27 +40,6 @@ pen.hideturtle()
 pen.goto(0, 260)
 pen.write("Score: {}".format(score_b), align="center", font=("Courier", 24, "normal")) #code for title and scoreboard
 
-# Function
-# def paddle_a_up():
-#     y = paddle_a.ycor()
-#     y += 20
-#     paddle_a.sety(y)
-
-# def paddle_a_down():
-#     y = paddle_a.ycor()
-#     y -= 20
-#     paddle_a.sety(y)
-
-# def paddle_b_up():
-#   y = paddle_b.ycor()
-#   y += 20
-#   paddle_b.sety(y)
-
-# def paddle_b_down):
-#    y = paddle_b.ycor()
-#    y -= 20
-#    paddle_b.sety(y)
-
 def paddle_b_left():
     x = paddle_b.xcor()
     x += -30
@@ -84,11 +53,6 @@ def paddle_b_right():
 
 # Keyboard binding
 screen.listen()
-#screen.onkeypress(paddle_a_up, "w")
-#screen.onkeypress(paddle_a_down, "s")
-#screen.onkeypress(paddle_b_up, "Up")
-#screen.onkeypress(paddle_b_down, "Down")
-
 screen.onkeypress(paddle_b_left, "Left")
 screen.onkeypress(paddle_b_right, "Right")
 
@@ -119,36 +83,14 @@ while True:
         ball.setx(-400)
         ball.dx *= -1
 
-    # Left and right
-    # if ball.xcor() > 350:
-    #     score_a += 1
-    #     pen.clear()
-    #     pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
-    #     ball.goto(0, 0)
-    #     ball.dx *= -1
 
-    # elif ball.xcor() < -350:
-    #     score_b += 1
-    #     pen.clear()
-    #     pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
-    #     ball.goto(0, 0)
-    #     ball.dx *= -1
-
-    if ball.ycor() <- 220:
+    if ball.ycor() <- 250:
         score_b += 1
         pen.clear()
         pen.write("Score: {}".format(score_b), align="center", font=("Courier", 24, "normal"))
         ball.goto(0,0)
         ball.dx *= -1
-
-      # Paddle and ball collisions
-    # if ball.xcor() < -340 and ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50:
-    #     ball.dx *= -1 
-            
-    # elif ball.xcor() > 340 and ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50:
-    #     ball.dx *= -1
-    
-    #Paddle and ball collisions for paddle B
-        if ball.ycor() < -190 and ball.xcor() < (paddle_b.xcor() + 50) and ball.xcor() > (paddle_b.xcor() - 50):
-            ball.dy *= -1
-            score_b +=1
+    #Paddle and ball collisions 
+    if  ball.ycor() < -190 and ball.xcor() < paddle_b.xcor() + 50 and ball.xcor() > paddle_b.xcor() - 50:
+        ball.sety(-190)
+        ball.dy *= -1
